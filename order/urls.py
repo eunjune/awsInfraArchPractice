04e0d5a85ba23c52525b96ec20c803353c466f5a
eunjune <iws1605@naver.com>
2023-01-08 01:10:35 +0900

    프로젝트 세팅

--- order/urls.py
+++ order/urls.py
@@ -20,6 +20,6 @@ from order import views
 
 urlpatterns = [
     path('shops/', views.shop, name="shop"),
-    path('menus/<int:shop>', views.menu, name="menu"),
+    # path('menus/<int:shop>', views.menu, name="menu"),
     path('order/',views.order, name="order")
 ]
