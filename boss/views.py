e3070e0e5be2afed3f8e90765ca6b233564d489d
eunjune <iws1605@naver.com>
2023-01-08 00:39:34 +0900

    프로젝트 세팅

--- boss/views.py
+++ boss/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from order.models import Shop,Menu, Order,Orderfood
-from order.serializers import ShopSerializer, MenuSerializer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
